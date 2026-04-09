612844298ac78c2e479f9b065b75a0673d063662
Yadav Subba <yadavsubba2003@gmail.com>
2026-04-09 20:41:26 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCDHDWhC-JkJQBSNxKAd7dWwX8agBYuuc",
+  apiKey: "AIzaSyCCdHDWh-CJkJQBSNxKAd7dWwX8agBYuuc",
   authDomain: "prompt-ai-a0c21.firebaseapp.com",
   projectId: "prompt-ai-a0c21",
   storageBucket: "prompt-ai-a0c21.firebasestorage.app",
@@ -12,6 +12,7 @@ const firebaseConfig = {
   measurementId: "G-B98ZKD5KP9"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
